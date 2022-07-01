3fe6de5755c87f86cd5e1246960572f6c7db071e
LuizFabiodoCarmo <104438927+LuizFabiodoCarmo@users.noreply.github.com>
2022-07-01 17:10:34 -0300

    Adicionando Componentes e Rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { OngsComponent } from './ongs/ongs.component';
+import { SobreComponent } from './sobre/sobre.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastrarComponent,
+    HomeComponent,
+    ProdutosComponent,
+    OngsComponent,
+    SobreComponent,
+    
   ],
   imports: [
     BrowserModule,
